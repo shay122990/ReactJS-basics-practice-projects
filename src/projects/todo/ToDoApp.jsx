05a816e5b23e78f18ee72a6c0d0db79407c6bffe
shay asanova <moeshakasanova@gmail.com>
2025-09-25 13:06:46 +0400

    todo app functionality

--- src/projects/todo/ToDoApp.jsx
+++ src/projects/todo/ToDoApp.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import { useState } from "react";
 
-const Todo = () => {
+export default function App() {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+
+  const addTask = () => {
+    if (!input.trim()) return;
+    setList((prevList) => [...prevList, input]);
+    setInput("");
+  };
+  const handleDelete = (indexToRemove) => {
+    setList((prevList) => prevList.filter((_, i) => i !== indexToRemove));
+  };
+
   return (
     <div className="panel">
-      <h1>To-Do App</h1>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            {item} <button onClick={() => handleDelete(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
-
-export default Todo;
+}
