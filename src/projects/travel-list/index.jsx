0ed33d3f6d0a95ad24e6d2f114445862f1aea227
shay asanova <moeshakasanova@gmail.com>
2026-06-02 12:53:49 +0400

    added clearList function to travel list app

--- src/projects/travel-list/index.jsx
+++ src/projects/travel-list/index.jsx
@@ -26,6 +26,12 @@ function App() {
     );
   }
 
+  function handleClearItems() {
+    const confirmed = window.confirm("Are you sure you want delete all items");
+
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className={styles.app}>
       <Logo />
@@ -34,6 +40,7 @@ function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearItems}
       />
       <Stats items={items} />
     </div>
@@ -93,11 +100,26 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className={styles.list}>
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -106,6 +128,15 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className={styles.actions}>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear list</button>
+      </div>
     </div>
   );
 }
