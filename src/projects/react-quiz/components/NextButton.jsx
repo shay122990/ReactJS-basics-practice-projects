2b403188cedcad232ae3502780fe4b40c3ae5163
shay asanova <moeshakasanova@gmail.com>
2026-07-23 18:50:25 +0400

    react-quiz: finish screen set up and restart quiz

--- src/projects/react-quiz/components/NextButton.jsx
+++ src/projects/react-quiz/components/NextButton.jsx
@@ -1,16 +1,26 @@
 import styles from "../ReactQuiz.module.css";
 
-function NextButton({ dispatch, answer }) {
+function NextButton({ dispatch, answer, numQuestions, index }) {
   if (answer === null) return null;
 
-  return (
-    <button
-      className={`${styles.btn} ${styles.btnUi}`}
-      onClick={() => dispatch({ type: "nextQuestion" })}
-    >
-      Next
-    </button>
-  );
+  if (index < numQuestions - 1)
+    return (
+      <button
+        className={`${styles.btn} ${styles.btnUi}`}
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+  if (index < numQuestions - 1)
+    return (
+      <button
+        className={`${styles.btn} ${styles.btnUi}`}
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
 }
 
 export default NextButton;
--- src/projects/react-quiz/index.jsx
+++ src/projects/react-quiz/index.jsx
@@ -8,6 +8,7 @@ import StartScreen from "./components/StartScreen";
 import Question from "./components/Question.jsx";
 import NextButton from "./components/NextButton.jsx";
 import Progress from "./components/Progress.jsx";
+import FinishScreen from "./components/FinishScreen.jsx";
 
 const initialState = {
   questions: [],
@@ -16,6 +17,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -40,17 +42,27 @@ function reducer(state, action) {
     }
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
-
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
     default:
       throw new Error("unknown action");
   }
 }
 
 export default function ReactQuiz() {
-  const [{ questions, status, answer, index, points }, dispatch] = useReducer(
-    reducer,
-    initialState,
-  );
+  const [{ questions, status, answer, index, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
@@ -95,8 +107,21 @@ export default function ReactQuiz() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
         )}
       </Main>
     </div>
